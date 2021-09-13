aa899136730d291bba2306355ea9861cea7e5b9a
Soluciones Cyberdine <70183721+cyberdine1976@users.noreply.github.com>
2021-09-13 17:21:32 +0200

    version 1

---
+++ Ecommerce.Dominio/ProductoDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Dominio
+{
+    public class ProductoDTO
+    {
+        public int IdArticulo { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public Nullable<int> Categoria { get; set; }
+        public Nullable<decimal> Precio { get; set; }
+
+    }
+}
---
+++ Ecommerce.Presentacion/Controllers/ProductoController.cs
@@ -0,0 +1,29 @@
+﻿using Ecommerce.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Presentacion.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Nuevo()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Nuevo(ProductoDTO producto)
+        {
+            return View();
+        }
+
+    }
+}
